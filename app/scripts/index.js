9641336acfdc262409f31c26c97cd8d7d4a137a6
Luis Rene Onate <lonate12@gmail.com>
2016-10-13 16:23:42 -0400

    Added templates file and are now generating a random opponent.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,17 +1,35 @@
 var $ = require('jquery');
 var _ = require('underscore');
 var models = require('./models');
-var userPlayer,
-    opponentPlayer;
 
 $(function(){
+  var userPlayer,
+      opponentPlayer;
+
+  var goodGuysArray = [
+    new models.GoodGuy({name:'Harry', imageURL: 'http://unsplash.it/40/40'}),
+    new models.GoodGuy({name:'Hermione', imageURL: 'http://unsplash.it/40/40'}),
+    new models.GoodGuy({name:'Hagrid', imageURL: 'http://unsplash.it/40/40'}),
+    new models.GoodGuy({name:'Professor Lupin', imageURL: 'http://unsplash.it/40/40'}),
+    new models.GoodGuy({name:'Professor Dumbledore', imageURL: 'http://unsplash.it/40/40'}),
+  ];
+
+  var badGuysArray = [
+    new models.BadGuy({name:'Clown', imageURL: 'http://unsplash.it/40/40'}),
+    new models.BadGuy({name:'Zombie', imageURL: 'http://unsplash.it/40/40'}),
+    new models.BadGuy({name:'Werewolf', imageURL: 'http://unsplash.it/40/40'}),
+    new models.BadGuy({name:'Dracula', imageURL: 'http://unsplash.it/40/40'}),
+    new models.BadGuy({name:'Ghost', imageURL: 'http://unsplash.it/40/40'}),
+    new models.BadGuy({name:'Spider', imageURL: 'http://unsplash.it/40/40'}),
+    new models.BadGuy({name:'Frankenstein', imageURL: 'http://unsplash.it/40/40'}),
+  ];
+
+  function pickOpponent(){
+    opponentPlayer = badGuysArray[Math.floor(Math.random()*badGuysArray.length)];
+    return opponentPlayer;
+  }
+
+  pickOpponent();
+
 
 });
-
-var userPlayer = new models.GoodGuy({name: 'Harry'});
-
-console.log(userPlayer);
-
-userPlayer.decreaseHealth();
-
-console.log(userPlayer);
